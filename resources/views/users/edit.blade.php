c8d7ab8f0cc3dbf8ce0717061d483572cb149a36
Julius Decena <julius.decena3095@gmail.com>
2023-07-04 10:31:49 +0800

    Update edit.blade.php

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -13,8 +13,6 @@
             </x-slot>
         </x-ui.page-header>
     </x-slot>
-
-
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
             <x-ui.card>
