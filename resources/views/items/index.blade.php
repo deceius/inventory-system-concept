ae09a4bb63994a9b053ff118bf3f6ab01735e6af
Julius Decena <julius.decena3095@gmail.com>
2023-08-15 19:48:41 +0800

    simplified search

--- resources/views/items/index.blade.php
+++ resources/views/items/index.blade.php
@@ -32,16 +32,8 @@
                           </svg>
                     </x-slot>
                     <x-slot:buttons>
-                        <div class="space-x-3 flex items-center h-5">
-                            <div class="w-full inline-flex items-center bg-white disabled:bg-gray-200 border border-gray-300 rounded-md text-xs text-gray-700 uppercase tracking-widest shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 disabled:opacity-25 transition ease-in-out duration-150">
-                                <input x-model="filter.search" x-on:keyup.enter="loadItems()" class="text-sm border-0 px-5 rounded-lg focus:ring-0" type="text" name="search" placeholder="Search">
-                                <button x-on:click="loadItems()" type="button" class="px-3 py-2">
-                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5">
-                                        <path fill-rule="evenodd" d="M9 3.5a5.5 5.5 0 100 11 5.5 5.5 0 000-11zM2 9a7 7 0 1112.452 4.391l3.328 3.329a.75.75 0 11-1.06 1.06l-3.329-3.328A7 7 0 012 9z" clip-rule="evenodd" />
-                                      </svg>
-                                </button>
-                              </div>
-                        </div>
+                        <x-ui.search model="filter.search" click-method="loadItems()"/>
+
                     </x-slot>
                     <x-slot:content ::class="{ 'opacity-50' : isLoading }">
                         <div class="overflow-x-auto">
